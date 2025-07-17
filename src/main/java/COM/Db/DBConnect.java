f77645562fd4a6e27151fb09b637e5c2c069fe98
whenyouwerelittle <whenyouwerelittle@gmail.com>
2025-07-17 18:45:14 +0900

    no topic

---
+++ src/main/java/COM/Db/DBConnect.java
@@ -0,0 +1,11 @@
+package COM.Db;
+
+import java.sql.Connection;
+
+public class DBConnect {
+    private Connection connection;
+    private String DB_NAME;
+    private String DB_USER;
+    private String DB_PASSWORD;
+    private int DB_PORT;
+}
--- src/main/java/COM/Main.java
+++ src/main/java/COM/Main.java
@@ -1,11 +1,8 @@
 package COM;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
           // 시작하면 모든 사용자 정보를 로그아웃 상태로 전환한다.
-            System.out.println("You are here");
+            System.out.println("You are started");
             Container.init();
             new App().run();
             Container.close();
